5c43eb73a447107830f33501a337a9d63871c5ce
姚国强 <2396034319@qq.com>
2023-10-27 15:41:33 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.hWindowControl1 = new HalconDotNet.HWindowControl();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.hWindowControl1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(775, 385);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // hWindowControl1
+            // 
+            this.hWindowControl1.BackColor = System.Drawing.Color.Black;
+            this.hWindowControl1.BorderColor = System.Drawing.Color.Black;
+            this.hWindowControl1.ImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hWindowControl1.Location = new System.Drawing.Point(195, 20);
+            this.hWindowControl1.Name = "hWindowControl1";
+            this.hWindowControl1.Size = new System.Drawing.Size(376, 359);
+            this.hWindowControl1.TabIndex = 0;
+            this.hWindowControl1.WindowSize = new System.Drawing.Size(376, 359);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "创建ROI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(177, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "创建模板";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(259, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "匹配模板";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private HalconDotNet.HWindowControl hWindowControl1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿
+using OpcUaHelper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+using static System.Windows.Forms.MonthCalendar;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        HWindow hw_home;
+        HObject ReadImage, GenRectangle1, ho_ModelContours;
+        HTuple hv_ModelID;
+        private HTuple hv_Area;
+        private HTuple hv_Row;
+        private HTuple hv_Column;
+        private HTuple hv_HomMat2D,hv_HomMat2DTranslate;
+        private HObject ho_ContoursAffineTrans;
+        private HTuple hv_Row1;
+        private HTuple hv_Column1;
+        private HTuple hv_Angle;
+        private HTuple hv_Score;
+        private HTuple hv_HomMat2DIdentity;
+        private HTuple hv_HomMat2DRotate;
+        private HObject ho_Rectangle1;
+        private HObject ho_ContoursAffineTrans1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fName = string.Empty;
+            using (OpenFileDialog open = new OpenFileDialog())
+            {
+               
+                DialogResult result = open.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK)
+                {
+                    fName = open.FileName;
+                }
+                else
+                    return;
+                HOperatorSet.ReadImage(out ReadImage, fName);
+                HOperatorSet.GetImageSize(ReadImage, out var Imagewidth, out var Imageheight);
+                HOperatorSet.SetPart(hw_home, 0, 0, Imagewidth - 1, Imageheight - 1);
+                HOperatorSet.DispObj(ReadImage, hw_home);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hw_home = hWindowControl1.HalconWindow;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            HOperatorSet.FindShapeModel(ReadImage, hv_ModelID, (new HTuple(-180)).TupleRad()
+       , (new HTuple(180)).TupleRad(), 0.5, 0, 0.5, "least_squares", 0, 0.9, out hv_Row1,
+       out hv_Column1, out hv_Angle, out hv_Score);
+            HOperatorSet.DispObj(ReadImage,hw_home);
+
+            for (int hv_i = 0; hv_i <= (int)((new HTuple(hv_Score.TupleLength())) - 1); hv_i = hv_i + 1)
+            {
+                HOperatorSet.HomMat2dIdentity(out hv_HomMat2DIdentity);
+               
+                  //  hv_HomMat2DTranslate.Dispose();
+                    HOperatorSet.HomMat2dTranslate(hv_HomMat2DIdentity, hv_Row1.TupleSelect(hv_i),
+                        hv_Column1.TupleSelect(hv_i), out hv_HomMat2DTranslate);
+                
+                    HOperatorSet.HomMat2dRotate(hv_HomMat2DTranslate, hv_Angle.TupleSelect(hv_i),
+                        hv_Row1.TupleSelect(hv_i), hv_Column1.TupleSelect(hv_i), out hv_HomMat2DRotate);
+                
+             
+              
+                    HOperatorSet.GenRectangle2(out ho_Rectangle1, hv_Row1.TupleSelect(hv_i), hv_Column1.TupleSelect(
+                        hv_i), hv_Angle.TupleSelect(hv_i), 30, 30);
+               
+           
+                    HOperatorSet.SetDraw(hw_home, "margin");
+             
+            
+                HOperatorSet.AffineTransContourXld(ho_ModelContours, out ho_ContoursAffineTrans1,
+                    hv_HomMat2DRotate);
+        
+      
+                    HOperatorSet.DispObj(ho_Rectangle1, hw_home);           
+                    HOperatorSet.DispObj(ho_ContoursAffineTrans1, hw_home );
+                
+            }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HOperatorSet.ReduceDomain(ReadImage, GenRectangle1, out var imageReduced);
+            HOperatorSet.CreateScaledShapeModel(imageReduced, "auto", (new HTuple(-180)).TupleRad()
+    , (new HTuple(180)).TupleRad(), "auto", 0.9, 1.1, "auto", "auto", "use_polarity",
+    "auto", "auto", out hv_ModelID);
+            HOperatorSet.GetShapeModelContours(out ho_ModelContours, hv_ModelID, 1);
+            HOperatorSet.AreaCenter(GenRectangle1, out hv_Area, out hv_Row, out hv_Column);
+            HOperatorSet.VectorAngleToRigid(0, 0, hv_Area, hv_Row, hv_Column, hv_Area, out hv_HomMat2D);
+            HOperatorSet.AffineTransContourXld(ho_ModelContours, out ho_ContoursAffineTrans,
+      hv_HomMat2D);
+            HOperatorSet.DispObj(ReadImage, hw_home);
+            HOperatorSet.DispObj(ho_ContoursAffineTrans, hw_home);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            HOperatorSet.GenEmptyObj(out GenRectangle1);
+            GenRectangle1.Dispose();
+            HOperatorSet.DispObj(ReadImage, hw_home);
+            HOperatorSet.SetColor(hw_home,"red" );
+            HOperatorSet.SetDraw(hw_home, "margin");
+            HOperatorSet.DrawRectangle1(hw_home,out var row1,out var col1 , out var row2, out var col2);    
+            HOperatorSet.GenRectangle1(out GenRectangle1, row1,col1,row2,col2);
+            HOperatorSet.DispObj(GenRectangle1,hw_home);
+        }
+    }
+}
